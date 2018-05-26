64fa12f2cba6c499ea059e1a0465c66b3c59c76a
ATalaveraDev <atalavera86@gmail.com>
2018-05-26 20:11:32 +0200

    R - removed unnecessary code

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -49,8 +49,4 @@ export class ListComponent {
       }
     });
   }
-
-  onDragEnter(event): void {
-    // this.elements.filter((element: Videogame) => element.name !== event.target.innerText);
-  }
 }
